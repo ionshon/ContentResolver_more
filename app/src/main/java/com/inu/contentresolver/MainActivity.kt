ca97881fa38e25147b92b24c73a6fcc382c75b99
ionshon <inushongg@gmail.com>
2021-10-17 16:47:42 +0900

    chuga

--- app/src/main/java/com/inu/contentresolver/MainActivity.kt
+++ app/src/main/java/com/inu/contentresolver/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.inu.contentresolver
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
@@ -14,7 +13,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.contentresolver.BaseActivity
 import com.inu.contentresolver.databinding.ActivityMainBinding
 import java.io.FileNotFoundException
+import java.lang.Exception
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 
+import android.os.ParcelFileDescriptor
+
+import android.content.ContentUris
+
+import android.content.ContentResolver
+import android.content.Context
+import java.io.IOException
 
 class MainActivity : BaseActivity() {
 
@@ -24,6 +33,11 @@ class MainActivity : BaseActivity() {
     val FLAG_REQ_STORAGE = 99
     val FLAG_PERM_STORAGE = 102
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    var albumarts = ArrayList<Bitmap>()
+    var albumInfo = ArrayList<String>()
+    private val sBitmapOptionsCache = BitmapFactory.Options()
+    private val sArtworkUri = Uri.parse("content://media/external/audio/albumart")
+    var mContext: Context? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +61,7 @@ class MainActivity : BaseActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
-    @SuppressLint("Recycle")
+
     fun getMusicList(): List<Music> {
         // 컨텐트 리졸버로 음원 목록 가져오기
         // 1. 데이터 테이블 주소
@@ -65,20 +79,24 @@ class MainActivity : BaseActivity() {
         // 4. 커서로 전달받은 데이터를 꺼내서 저장
         val musicList = mutableListOf<Music>()
         var i=0
+
         while (cursor?.moveToNext() == true) {
             val id = cursor.getString(0)
             val title = cursor.getString(1)
             val artist = cursor.getString(2)
             val albumId = cursor.getString(3) //Long = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)) //cursor!!.getString(3)
-            val albumUri = Uri.parse("content://media/external/audio/albumart/$albumId")
+            var albumUri = Uri.parse("content://media/external/audio/albumart/$albumId")
             val duration = cursor.getLong(4)
 
-            val defaultUri = Uri.parse("android.resource://com.inu.contentresolver/drawable/next_thin")
-            if( contentResolver.openFileDescriptor(albumUri, "r") == FileNotFoundException())
-            {
+          //  val defaultUri = Uri.parse("android.resource://com.inu.contentresolver/drawable/next_thin")
+
+
+            try {
+               var fd =  contentResolver.openFileDescriptor(albumUri, "r")
+            } catch (e : Exception) {
                 i+=1
-                val albumUri = defaultUri
-                Log.w("파일디스크립터", "something error!! $i");
+                var albumUri = Uri.parse("android.resource://com.inu.contentresolver/drawable/next_thin")
+                Log.d("예외", "something error!! $i")
             }
             // var fd: ParcelFileDescriptor?  = contentResolver.openFileDescriptor(albumUri, "r") ?: contentResolver.openFileDescriptor(defaultUri, "r")
 
@@ -89,6 +107,96 @@ class MainActivity : BaseActivity() {
         }
         return  musicList
     }
+
+    fun getMusicLIst2() {
+        val cols = arrayOf(
+            MediaStore.Audio.Albums._ID,
+            MediaStore.Audio.Albums.ARTIST,
+            MediaStore.Audio.Albums.ALBUM,
+            MediaStore.Audio.Albums.ALBUM_ART,
+
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ALBUM_ID, // 추가
+            MediaStore.Audio.Media.DURATION
+        )
+
+        val cur = contentResolver.query(
+            MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+            cols, null, null, null
+        )
+        if (cur?.moveToFirst() == true) {
+            var ablum: String
+            var artist: String
+            var id: String
+            var albumArt: String
+            var idNum: Int
+
+            var title: String
+            var albumId: String  // 추가
+            var duration: Long
+
+            val artistColumn: Int = cur.getColumnIndex(MediaStore.Audio.Albums.ARTIST)
+            val ablumColumn: Int = cur.getColumnIndex(MediaStore.Audio.Albums.ALBUM)
+            val albumArtColumn: Int = cur.getColumnIndex(MediaStore.Audio.Albums.ALBUM_ART)
+           // val titleColumn: Int = cur.getColumnIndex((MediaStore.Audio.Albums))
+            var imagePath: String
+            do {
+                // Get the field values
+                idNum = cur.getInt(0) // 레코드의 _id를 가져옵니다.
+                id = idNum.toString()
+                artist = cur.getString(artistColumn) // title 필드의 값을 가져옴
+                ablum = cur.getString(ablumColumn) // artist 필드의 값을 가져옴
+                albumArt = cur.getString(albumArtColumn)
+                val album_art: Bitmap? = getBitmapImage(idNum, 200, 200)
+                albumarts.add(album_art!!) // 두개 합쳐서  array list 에 넣어줌.
+                albumInfo.add("$ablum - $artist")
+            } while (cur.moveToNext())
+        }
+    }
+    fun getBitmapImage(id: Int, w: Int, h: Int): Bitmap? {
+        val res: ContentResolver = contentResolver // mContext!!.getContentResolver()
+        val uri = ContentUris.withAppendedId(sArtworkUri, id.toLong())
+        if (uri != null) {
+            var fd: ParcelFileDescriptor? = null
+            return try {
+                fd = res.openFileDescriptor(uri, "r")
+                var sampleSize = 1
+                sBitmapOptionsCache.inJustDecodeBounds = true
+                BitmapFactory.decodeFileDescriptor(fd!!.fileDescriptor, null, sBitmapOptionsCache)
+                var nextWidth: Int = sBitmapOptionsCache.outWidth shr 1
+                var nextHeight: Int = sBitmapOptionsCache.outHeight shr 1
+                while (nextWidth > w && nextHeight > h) {
+                    sampleSize = sampleSize shl 1
+                    nextWidth = nextWidth shr 1
+                    nextHeight = nextHeight shr 1
+                }
+                sBitmapOptionsCache.inSampleSize = sampleSize
+                sBitmapOptionsCache.inJustDecodeBounds = false
+                var b = BitmapFactory.decodeFileDescriptor(
+                    fd.fileDescriptor, null, sBitmapOptionsCache
+                )
+                if (b != null) {
+                    if (sBitmapOptionsCache.outWidth !== w || sBitmapOptionsCache.outHeight !== h) {
+                        val tmp = Bitmap.createScaledBitmap(b, w, h, true)
+                        b.recycle()
+                        b = tmp
+                    }
+                }
+                b
+            } catch (e: FileNotFoundException) {
+                null
+            } catch (e: Exception) {
+                null
+            } finally {
+                try {
+                    fd?.close()
+                } catch (e: IOException) {
+                }
+            }
+        }
+        return null
+    }
+
 
     fun isPermitted() : Boolean { // 책에는 checkPermission, 조건이 하나일 때 한줄로
         return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
